7913b6222eed35f4ec44a41edfda5a337098d4bb
mahimtabassum15 <brightthoughtsoffm@gmail.com>
2025-05-23 01:43:30 +0600

    update

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+    $name="Mahim Tabassum";
+    // $food="Barger";
+    // $email="mahimtabassum406@gmail.com";
+
+
+    echo "Hello {$name}<br>";
+    // echo "You Like {$food}<br>";
+    // echo "Your email is {$email}<br>";
+
+?>
+<!-- Get Method -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- <form action="index.php" method="get">
+    <label for="#">username:</label><br>
+    <input type="text" name="username" id=""><br>
+    <label for="#">password:</label><br>
+    <input type="password" name="password" ><br>
+    <input type="submit" value="Login">
+
+    </form>
+
+    Post Method  it's secure
+
+    <form action="index.php" method="post">
+    <label for="#">username:</label><br>
+    <input type="text" name="username" id=""><br>
+    <label for="#">password:</label><br>
+    <input type="password" name="password" ><br>
+    <input type="submit" value="Login">
+    </form> -->
+
+    <!-- <form action="index.php" method="post">
+        <label for="">Quantity: </label><br>
+        <input type="text" name="quantity"> 
+        <input type="submit" value="Total">
+    </form> -->
+</body>
+</html>
+<?php
+
+    // // Form php code start
+
+    // // new liner er jonno nicher 2tor ekta korle hoy
+    // echo $_GET["username"] ."<br>";
+    // echo "{$_GET["password"]} <br>";
+
+    // // new liner er jonno nicher 2tor ekta korle hoy
+    // echo $_POST["username"] ."<br>";
+    // echo "{$_POST["password"]} <br>";
+
+    // // Form php code End
+    
+    // $item = "pizza";
+    // $price = 10.66;
+    // $quantity  = $_POST["quantity"];
+    // $total = null;
+
+    // $total = $quantity * $price;
+    // echo"You Have Orderes {$quantity} x {$item}/s <br>";
+    // echo"your Total is : \${$total}";
+
+?>
+
+
+

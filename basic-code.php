9d60eca6036169689f2da9b6d1533e3584a7eda8
mahimtabassum15 <brightthoughtsoffm@gmail.com>
2025-05-24 02:45:23 +0600

    update

--- basic-code.php
+++ basic-code.php
@@ -1,13 +1,30 @@
 <?php
+//Vriable
 $name = "Mahim";
 $age = 23;
-?>
 
-<?php
+//Print
+echo "Hello, World! <br>";
+print "Welcome to PHP! <br>";
+
+//Constant
 define("SITE_NAME", "M-IT");
-echo SITE_NAME;
+echo SITE_NAME . "<br>";
+
+// Arithmetic Operation
+echo "A = " . $a = 10 . "<br>";
+echo "B = " . $b = 5 . "<br>";
+
+// Addition
+echo "Addition = " . $a + $b ."<br>"; // 15
+
+// Subtraction
+echo "Substraction = " . $a - $b . "<br>"; // 5
+
+// Multiplication
+echo "Multiplication = " . $a * $b . "<br>"; // 50
+
+// Division
+echo "Divission = " . $a / $b . "<br>"; // 2
+
 ?>
-
-
-<!-- echo "Hello, World!";
-print "Welcome to PHP!"; -->
